17d5f7700a344081973509ccc63268ae684ef2c3
Victor Pilotto <victor@acruxx.com.br>
2018-11-12 21:00:16 -0200

    Second Commit

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,8 +6,6 @@ class UserTest extends TestCase
 {
     public function testReturnsFullName()
     {
-        require 'User.php';
-
         $user = new User;
 
         $user->first_name   = "Teresa";
